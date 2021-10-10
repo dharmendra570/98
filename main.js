3a2d670cefaca5917ff73615df05eeb9187bf130
dharmendra570 <69644062+dharmendra570@users.noreply.github.com>
2021-10-10 17:31:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if (Content == "take my selfie") {
+        console.log("take my selfie ---");
+        speak();
+    }
+
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking you Selfie in 5 seconds";
+
+    var UtterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(UtterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function () {
+        take_snapshot();
+        save();
+    }, 5000);
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '">';
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
